544350c7fe120eb9990fe667393526262c5a7d59
Tara Denniston <taradenniston@gmail.com>
2024-03-09 14:33:06 -0500

    add GET route for /[code]

--- routes/invoices.js
+++ routes/invoices.js
@@ -14,13 +14,39 @@ router.get('/', async (req, res, next) => {
   }
 })
 
-// router.get('/:id', async (req, res, next) => {
-//   try {
+router.get('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
 
-//   } catch (e) {
-//     return next(e);
-//   }
-// })
+    // Get data on invoice and company
+    const results = await db.query(`SELECT i.id, i.comp_code, i.amt, 
+      i.paid, i.add_date, i.paid_date, c.name, c.description FROM invoices 
+      AS i JOIN companies AS c ON (i.comp_code = c.code) WHERE id=$1`, 
+      [id]);
+    console.log(results.rows[0]);
+    if (results.rows.length === 0) {
+      throw new ExpressError(`Invoice ID '${id}' does not exist`, 404)
+    }
+    const data = results.rows[0];
+
+    // Format response to return
+    const invoice = {
+      id: data.id,
+      amt: data.amt,
+      paid: data.paid,
+      add_date: data.add_date,
+      paid_date: data.paid_date,
+      company: {
+        code: data.comp_code,
+        name: data.name,
+        description: data.description
+      }
+    }
+    return res.json({ invoice: invoice });
+  } catch (e) {
+    return next(e);
+  }
+})
 
 // router.post('/', async (req, res, next) => {
 //   try {
