ae5b4bcdb413c1e7279d81505e946aeded303677
Tara Denniston <taradenniston@gmail.com>
2024-03-09 14:54:27 -0500

    add POST route for /

--- routes/invoices.js
+++ routes/invoices.js
@@ -48,13 +48,17 @@ router.get('/:id', async (req, res, next) => {
   }
 })
 
-// router.post('/', async (req, res, next) => {
-//   try {
-
-//   } catch (e) {
-//     return next(e);
-//   }
-// })
+router.post('/', async (req, res, next) => {
+  try {
+    const { comp_code, amt } = req.body;
+    const results = await db.query(`INSERT INTO invoices (comp_code, amt) 
+      VALUES ($1, $2) RETURNING id, comp_code, amt, paid, add_date, 
+      paid_date`, [comp_code, amt]);
+    return res.json({invoice: results.rows[0]});
+  } catch (e) {
+    return next(e);
+  }
+})
 
 // router.put('/:id', async (req, res, next) => {
 //   try {
