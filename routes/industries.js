57979084a7345491e0150820ffa7d196e2931c89
Tara Denniston <taradenniston@gmail.com>
2024-04-02 18:53:51 -0400

    add GET route for /

--- routes/industries.js
+++ routes/industries.js
@@ -4,3 +4,30 @@ const express = require('express');
 const ExpressError = require('../expressError');
 const router = express.Router();
 const db = require('../db');
+
+
+router.get('/', async (req, res, next) => {
+  try {
+    // Get all industries and related companies
+    const indResults = await db.query(`SELECT * FROM industries`);
+    const industries = indResults.rows;
+    const compResults = await db.query(`SELECT * from companies_industries`);
+    const indCompanies = compResults.rows;
+
+    // Add company codes to each industry
+    industries.map( item => {
+      item.companies = [];
+      indCompanies.map( c => {
+        if (item.code === c.ind_code) {
+          item.companies.push(c.comp_code);
+        }
+      });
+    });
+    return res.json({ industries: industries })
+  } catch (e) {
+    return next(e);
+  }
+})
+
+
+module.exports = router;
